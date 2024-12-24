f39ef72b1e5da0d2d20e588a1e14fcc4e8f40e22
Sylvain Hamann <sylvhama@gmail.com>
2024-12-24 10:21:16 -0500

    add theme toggler

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# My Final Fantasy VII collection
+A website to showcase my Final Fantasy VII collection.
 
 ## Inspiration
 
 - favicon: https://ramsesc.gumroad.com/l/finalfantasy
 - View transitions: https://tympanus.net/codrops/2023/10/03/animating-multi-page-navigations-with-browser-view-transitions-and-astro/
 - i18n: https://docs.astro.build/en/guides/internationalization/ and https://docs.astro.build/en/recipes/i18n/
+- Theme picker: https://ui.shadcn.com/examples/forms/appearance
--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -2,10 +2,24 @@ export const translations = {
   en: {
     title: "Final Fantasy VII Collection",
     description: "A gallery of all my Final Fantasy 7 collectibles",
+    madeBy: "Made by",
+    settings: "Settings",
+    pickTheme: "Pick website theme",
+    darkHeading: "Dark",
+    darkSubHeading: "Theme",
+    lightHeading: "Light",
+    lightSubHeading: "Theme",
   },
   fr: {
     title: "Collection Final Fantasy VII",
     description:
       "Une galerie de tous mes objets en rapport avec Final Fantasy 7",
+    madeBy: "Fait par",
+    settings: "Paramètres",
+    pickTheme: "Choisissez le thème du site",
+    darkHeading: "Thème",
+    darkSubHeading: "Sombre",
+    lightHeading: "Thème",
+    lightSubHeading: "Clair",
   },
 } as const;
