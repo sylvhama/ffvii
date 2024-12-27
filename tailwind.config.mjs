61b73694a5e341b56eacc79809d0b0b724dc35ce
Sylvain Hamann <sylvhama@gmail.com>
2024-12-27 10:54:14 -0500

    add display transition

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -25,6 +25,7 @@ export default {
   plugins: [
     plugin(function ({ addVariant }) {
       addVariant("ff7", [".ff7 & ", "&.ff7"]);
+      addVariant("starting", "@starting-style");
     }),
   ],
   darkMode: "class",
