83dffa6665290b62191e900186dc6a60d1a9e6b6
Sylvain Hamann <sylvhama@gmail.com>
2025-01-27 19:17:38 -0500

    add scroll test

---
+++ tests/post.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("scroll to main image on small screen", async ({ page, isMobile }) => {
+  test.skip(isMobile);
+  await page.setViewportSize({ width: 400, height: 400 });
+  await page.goto("games/final-fantasy-vii-ps1/");
+  await page.mouse.wheel(0, 9999);
+
+  await expect(page.getByAltText("front").first()).not.toBeInViewport();
+
+  await page.getByLabel("Pick back").click();
+
+  await expect(page.getByAltText("back").first()).toBeInViewport();
+});
