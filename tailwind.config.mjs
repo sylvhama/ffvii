0811dd4bd555272a971e136344aa927f8f340f66
Sylvain Hamann <sylvhama@gmail.com>
2025-02-01 17:34:37 -0500

    move colors and set theme color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,17 +6,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        realWhite: "#ffffff",
-        realBlack: "#000000",
-        white: "#f8f8f2",
-        black: "#1d1e26",
-        purple: "#7357ff",
-        pink: "#ff80bf",
-        blue: "#80ffea",
-        cyan: "#20B2AA",
-        ff7Pink: "#ffaaaa",
-      },
       fontFamily: {
         mono: ['"Lucida Console"', "Courier", ...defaultTheme.fontFamily.mono],
       },
@@ -29,7 +18,6 @@ export default {
   plugins: [
     plugin(function ({ addVariant }) {
       addVariant("ff7", [".ff7 & ", "&.ff7"]);
-      addVariant("starting", "@starting-style");
     }),
   ],
   darkMode: "class",
