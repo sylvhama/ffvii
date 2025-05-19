0bb8fc75ffd80acc1234217cb8df53b7a01df4e8
Sylvain Hamann <sylvhama@gmail.com>
2025-05-19 14:56:50 -0400

    update packages

--- tests/redirect.spec.ts
+++ tests/redirect.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from "@playwright/test";
 
 test("games redirect", async ({ page }) => {
   await page.goto("/games");
-  await page.waitForURL("https://ff7.rocks/");
-  expect(page.url()).toBe("https://ff7.rocks/");
+  await page.waitForURL("http://localhost:4321");
+  expect(page.url()).toBe("http://localhost:4321/");
 });
 
 test("fr/games redirect", async ({ page }) => {
   await page.goto("fr/games");
-  await page.waitForURL("https://ff7.rocks/fr/");
-  expect(page.url()).toBe("https://ff7.rocks/fr/");
+  await page.waitForURL("http://localhost:4321/fr");
+  expect(page.url()).toBe("http://localhost:4321/fr");
 });
 
 test("French page redirection when browser is French", async ({
